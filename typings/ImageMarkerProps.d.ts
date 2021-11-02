949cd64ede3e5b6075e834fb0f748d0b73723791
Joe Robertson <32719006+joerob319@users.noreply.github.com>
2021-11-02 10:35:06 +0000

    git ignore

---
+++ typings/ImageMarkerProps.d.ts
@@ -0,0 +1,45 @@
+/**
+ * This file was generated from ImageMarker.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+import { CSSProperties } from "react";
+import { DynamicValue, ListValue, WebImage } from "mendix";
+
+export interface ImageMarkerContainerProps {
+    name: string;
+    class: string;
+    style?: CSSProperties;
+    tabIndex?: number;
+    image: DynamicValue<WebImage>;
+    columns: number;
+    rows: number;
+    showGrid: boolean;
+    showMarkUp: boolean;
+    lowLimit: number;
+    highLimit: number;
+    data: ListValue;
+    pointColor: string;
+    lineColor: string;
+    lowColor: string;
+    medColor: string;
+    highColor: string;
+}
+
+export interface ImageMarkerPreviewProps {
+    class: string;
+    style: string;
+    image: string;
+    columns: number | null;
+    rows: number | null;
+    showGrid: boolean;
+    showMarkUp: boolean;
+    lowLimit: number | null;
+    highLimit: number | null;
+    data: {} | null;
+    pointColor: string;
+    lineColor: string;
+    lowColor: string;
+    medColor: string;
+    highColor: string;
+}
